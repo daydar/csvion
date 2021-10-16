0d9c76729b97749d2cede135960b943429c8662c
Deniz Aydar <deniz.aydar@gmx.de>
2021-10-17 00:30:27 +0200

    feat: add first unit test

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,17 @@ fn rocket() -> _ {
     .mount("/", routes![index, routes::data_routes::csv_data_raw])
     .mount("/data", FileServer::from(relative!("data")))
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn check_csv_raw() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+}
